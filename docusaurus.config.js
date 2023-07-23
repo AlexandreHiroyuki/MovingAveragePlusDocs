112607df8bb7f338912384afd1df5c528f0037b5
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2023-07-22 21:18:34 -0300

    fix: :bug: fix base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'MovingAveragePlus Docs',
   url: 'https://alexandrehiroyuki.github.io',
-  baseUrl: '/',
+  baseUrl: '/MovingAveragePlusDocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/icon.png',
